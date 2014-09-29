e5c69e6308a250cfc1898a6bfcec4f9394e3b612
Carl Lin <turkey039@gmail.com>
2014-09-29 16:02:54 +0800

    fix bugs in function- recoverForm

--- validation.js
+++ validation.js
@@ -1,31 +1,42 @@
 /*========================================================================================================== 
 Author: Carl  Lin
 Time: Sep, 2014
-Description:   Since jquery.validation is suck!!!!  Rewrite one with more simple function
+Description:   
+ 	Since jquery.validation is suck!!!!  Decide to rewrite one
 ===========================================================================================================*/
 if(typeof module === 'object'){
 	module.export = FormValidation;
 }
 
 var FormValidation = (function(window, document, my){
-	var recoverForm = function(){
+	my.recoverForm = function(){
 		///recover all we do at init
 		var warning = document.getElementById('required-warning');
-		warning.parentNode.removeChild(warning);
-
-		[].forEach.call(document.getElementsByClassName('validation-required'), function(e){
+		if( warning ){
+			warning.parentNode.removeChild(warning);
+		}
+		[].forEach.call(document.querySelectorAll('.validation-required'), function(e){
 			e.classList.remove('validation-required');
 		})
 
 	};
 
-///now only can input form class, will add input form id. in put for id like #xxx.
-	my.onsubmit = function( formClass, requireList){
+/*
+description: check if required field is empty & if retype is the same
+not yet finish: Now only can input form class, will add input form id. in put for id like #xxx.
+*/
+	my.onsubmit = function( formClass, requireList, repeatList){
 			var l = requireList.length,
+					i,
 					element = document.getElementsByClassName( formClass ),
 					elementLength = element.length,
 					elementCounting,
 					flag = true;
+
+			// var repeatTemp1 = '', ///variable for check repeat;
+			// 	repeatTemp2 = '',
+			// 	repeatLength = 0,
+			// 	repeatDefault = false;
 
 			var warningArr = [],///variables for set warning span
 					warningLength,
@@ -36,7 +47,7 @@ var FormValidation = (function(window, document, my){
 			for( var elementCounting=0; elementCounting<elementLength; elementCounting++ ){
 				///iterate all input in form
 				[].forEach.call(element[elementCounting].querySelectorAll('input'), function(e){
-					for( var i=0; i<l; i++ ){
+					for( i=0; i<l; i++ ){
 					///add validation-require class to empty input.
 						if( e.name === requireList[i] ){
 							if( e.value==='' ){
@@ -47,15 +58,16 @@ var FormValidation = (function(window, document, my){
 							}
 						}
 					}
-					///append  span with 'xxx is required'
-					if(warningContent !== ''){
-						var warningDiv = document.createElement('div');
-						warningDiv.id = 'required-warning';
-						warningDiv.innerHTML = warningContent;
-						element[elementCounting].insertBefore( warningDiv, element[elementCounting].childNodes[2] )
-						warningContent = '';
-					}
 				});
+
+				///append  span with 'xxx is required'
+				if(warningContent !== ''){
+					var warningDiv = document.createElement('div');
+					warningDiv.id = 'required-warning';
+					warningDiv.innerHTML = warningContent;
+					element[elementCounting].insertBefore( warningDiv, element[elementCounting].childNodes[2] )
+					warningContent = '';
+				}
 			}
 			return flag;// for stop submit
 	}
